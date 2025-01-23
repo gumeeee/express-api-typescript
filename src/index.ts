9365d9a8ba88b6316c89a98c49b9b77f466ee82c
gumeeee <gaxco123@gmail.com>
2025-01-23 16:00:15 -0300

    feat: add express and basic initial server

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
